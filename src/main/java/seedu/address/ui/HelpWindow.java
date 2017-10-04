62bed40481343420a20dfc6ad503cc906bea3c69
liliwei25 <liliwei25@hotmail.com>
2017-10-04 22:47:50 +0800

    Update HelpWindow.java

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -32,7 +32,7 @@ public class HelpWindow extends UiPart<Region> {
         Scene scene = new Scene(getRoot());
         //Null passed as the parent stage to make it non-modal.
         dialogStage = createDialogStage(TITLE, null, scene);
-//        dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
+        //dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
         dialogStage.setMaximized(false);
         FxViewUtil.setStageIcon(dialogStage, ICON);
 
