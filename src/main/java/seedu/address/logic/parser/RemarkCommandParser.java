d698cb7fa915e57e78ccae6cb207bbd0cff6eab8
liliwei25 <liliwei25@hotmail.com>
2017-10-05 02:22:08 +0800

    Update RemarkCommandParser.java

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -20,7 +20,7 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
      *
      * @throws ParseException if the user input does not conform the expected format
      */
-    public RemarkCommand parse(String args) throws ParseException{
+    public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
