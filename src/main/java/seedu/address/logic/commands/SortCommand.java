410bf627f08e12bb82a5bbf3b12dbfce972f066a
E0032186\JUSTIN TAN <e0032186@u.nus.edu>
2017-10-06 20:59:34 +0800

    fix travis 2

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -3,7 +3,7 @@ package seedu.address.logic.commands;
 /**
  * Sorts a the list of persons in alphabetical order
  */
-public class SortCommand extends Command{
+public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
     public static final String MESSAGE_SORT_SUCCESS = "Sorted in alphabetical order";
 
