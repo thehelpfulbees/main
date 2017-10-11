e9afb905feab7233e6a58878e4020bc36159215a
liliwei25 <liliwei25@hotmail.com>
2017-10-12 02:13:50 +0800

    Update Birthday.java

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -12,13 +12,16 @@ public class Birthday {
 
     public static final String MESSAGE_BIRTHDAY_CONSTRAINTS =
             "Birthdays can only contain numbers, and should be in the format dd-mm--yyyy";
-    public static final String BIRTHDAY_VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1" +
-            "|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\" +
-            "/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468]" +
-            "[048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\" +
-            "4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+    public static final String BIRTHDAY_VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1"
+            + "|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\"
+            + "/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468]"
+            + "[048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\"
+            + "4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
     public final String value;
-    public final int day, month, year;
+    public final int day; //Not implemented yet
+    public final int month;
+    public final int year;
+    
     /**
      * Validates given birthday.
      *
@@ -27,7 +30,7 @@ public class Birthday {
     public Birthday(String birthday) throws IllegalValueException {
         requireNonNull(birthday);
         String trimmedBirthday = birthday.trim();
-        if(birthday.equals("") || birthday.equals("Not Set") || birthday.equals("remove")) {
+        if (birthday.equals("") || birthday.equals("Not Set") || birthday.equals("remove")) {
             this.value = "Not Set";
             day = month = year = 0;
         } else if (!isValidBirthday(trimmedBirthday)) {
