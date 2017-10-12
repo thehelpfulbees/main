1116978c53c68b02cffb60b66290bb8d6de155d2
E0032186\JUSTIN TAN <e0032186@u.nus.edu>
2017-10-12 13:55:34 +0800

    updated tests

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.Test;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class SortCommandParserTest {
+    private SortCommandParser parser = new SortCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSortCommand() {
+        try {
+            SortCommand newCommand = parser.parse("num");
+            if (newCommand.equals(new SortCommand("num"))) {
+                assert true;
+            } else {
+                assert false;
+            }
+        } catch (ParseException pe) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+}
