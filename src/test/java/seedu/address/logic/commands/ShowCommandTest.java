6f5ab83e9677d7da0a0095b56e94b49869c0d969
Jo Brough <fr3d3rickfr3d3rickson@gmail.com>
2017-10-15 16:19:13 +0800

    Fixed ShowCommandTest – one case had an erroneous expected output. (I thought that only two of the test people were tagged as 'friends', but in fact all seven of them were)

--- src/test/java/seedu/address/logic/commands/ShowCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ShowCommandTest.java
@@ -66,9 +66,10 @@ public class ShowCommandTest {
 
     @Test
     public void execute_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 7);
         ShowCommand command = prepareCommand("friends");
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(ALICE, BENSON));
+        assertCommandSuccess(command, expectedMessage,
+                Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
     }
 
     /**
