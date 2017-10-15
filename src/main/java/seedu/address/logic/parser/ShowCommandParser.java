c82c8c0aad7041f49eef0c0e9ca6e7ffc6cd4af1
Jo Brough <fr3d3rickfr3d3rickson@gmail.com>
2017-10-15 14:15:56 +0800

    Added ShowCommand which will be used to search by tag. Currently is a copy of Find

---
+++ src/main/java/seedu/address/logic/parser/ShowCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.ShowCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new ShowCommand object
+ */
+public class ShowCommandParser implements Parser<ShowCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ShowCommand
+     * and returns an ShowCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ShowCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        String[] tagKeywords = trimmedArgs.split("\\s+");
+
+
+
+        return new ShowCommand(new NameContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
+    }
+
+}
