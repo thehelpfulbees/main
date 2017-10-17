1f9257d9b916c8d067f0e225ef482c40b0731556
Jo Brough <fr3d3rickfr3d3rickson@gmail.com>
2017-10-17 16:12:16 +0800

    Fixed import formatting for Travis

--- src/test/java/seedu/address/logic/commands/ShowCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ShowCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
