b6e76e0707bda69163797fb6b7228d1ef2d5b2ad
Jo Brough <fr3d3rickfr3d3rickson@gmail.com>
2017-10-25 20:17:32 +0800

    Added 'show' command's functionality to 'find'

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.TagsContainKeywordPredicate;
+
+import java.util.function.Predicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -16,9 +20,9 @@ public class FindCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final Predicate<ReadOnlyPerson> predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(Predicate<ReadOnlyPerson> predicate) {
         this.predicate = predicate;
     }
 
@@ -31,7 +35,12 @@ public class FindCommand extends Command {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof FindCommand // instanceof handles nulls
-                && this.predicate.equals(((FindCommand) other).predicate)); // state check
+                || (other instanceof FindCommand) // other is FindCommand AND...
+                && (((this.predicate instanceof NameContainsKeywordsPredicate) //it has a matching Name...Predicate OR
+                && (((FindCommand) other).predicate) instanceof NameContainsKeywordsPredicate)
+                && (this.predicate.equals(((FindCommand) other).predicate)))
+                || (((this.predicate instanceof TagsContainKeywordPredicate) //it has a matching Tag...Predicate
+                && (((FindCommand) other).predicate) instanceof TagsContainKeywordPredicate)
+                && (this.predicate.equals(((FindCommand) other).predicate))); // state check
     }
 }
